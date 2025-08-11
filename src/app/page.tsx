3c1270e60ccdb0a11ee273654ba09ce583f905c9
itx-alihamza <itx.alihamza.pro@gmail.com>
2025-08-11 16:02:40 +0500

    footer removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -114,37 +114,6 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className="min-w-screen text-white bg-[url(/assets/header_bg_mb.png)] md:bg-[url(/assets/header_bg.png)]">
-        <div className="w-full flex flex-col justify-around items-center lg:items-start lg:flex-row gap-10 px-8 py-10">
-          <div className="flex flex-col gap-4 justify-center items-center lg:items-start  px-[clamp(10px_3vw+20px_100px)]">
-            <h1 className="responsive-heading text-2xl font-extrabold">TheWorkPlace</h1>
-            <p className="responsive-body">Hayatabad, Phase IV, Peshawar, Pakistan</p>
-          </div>
-          <div className="flex flex-col gap-4 items-center ">
-            <h1 className="font-extrabold responsive-heading">Quick Links</h1>
-            <ul className="flex flex-col gap-2 list-disc font-light">
-              <li className="underline cursor-pointer responsive-body">Home</li>
-              <li className="underline cursor-pointer responsive-body">About Us</li>
-              <li className="underline cursor-pointer responsive-body">Out Project</li>
-              <li className="underline cursor-pointer responsive-body">Pricing</li>
-              <li className="underline cursor-pointer responsive-body">Contact</li>
-            </ul>
-          </div>
-          <div className="flex flex-col gap-4 items-center ">
-            <h1 className="font-extrabold responsive-heading">Important Links</h1>
-            <ul className="flex flex-col gap-2 list-disc font-light">
-              <li className="underline cursor-pointer responsive-body">Terms And Conditions</li>
-              <li className="underline cursor-pointer responsive-body">Legal</li>
-              <li className="underline cursor-pointer responsive-body">Business</li>
-              <li className="underline cursor-pointer responsive-body">Partners</li>
-            </ul>
-          </div>
-        </div>
-        <div className="w-full flex justify-center items-center border-t-[1px] px-20 py-4">
-          <p className="responsive-body">© Copyright 2025 Astechsolutions</p>
-        </div>
-
-      </section>
     </div>
   );
 }
